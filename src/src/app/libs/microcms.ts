439d37ba5565e2636c4e946b470b4d2da100c3e9
RyuRyuDX <gibson.red.gin.suke@gmail.com>
2024-03-17 21:47:15 +0900

    変数変更

--- src/src/app/libs/microcms.ts
+++ src/src/app/libs/microcms.ts
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: "ruru",
-  apiKey: "2fde1a54-efe3-4cb2-86e2-e74cb1b37e5a",
+  serviceDomain: process.env.SERVICE_DOMAIN!,
+  apiKey: process.env.API_KEY!,
 });
