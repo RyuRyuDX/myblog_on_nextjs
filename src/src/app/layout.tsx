5459ba74c367f056e5afd7c144aee05653071c46
RyuRyuDX <gibson.red.gin.suke@gmail.com>
2024-03-21 15:12:39 +0900

    Google Analytics追加

--- src/src/app/layout.tsx
+++ src/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "./components/GoogleAnalytics/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <GoogleAnalytics />
+      </head>
       <body
         className={`${inter.className} flex justify-center rounded-md bg-gray-900 text-white min-h-screen`}
       >
